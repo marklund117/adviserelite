608ba88bccd962f04c504a9fb6229d83166c9eae
Mark Lund <marklund117@gmail.com>
2024-11-28 15:25:43 -0700

    done with disclaimers page

--- app/disclaimers/page.tsx
+++ app/disclaimers/page.tsx
@@ -15,14 +15,18 @@ export default function Disclaimers() {
     <p>Watching, listening to, or reading content (video, audio, visual, or text) created by ADVISER ELITE or commenting on messages, or other communication that you send to ADVISER ELITE does not create a service provider-client relationship, and any information transmitted will not be protected by privilege or confidentiality.  ADVISER ELITE does not make any representation, warranty, or guarantee about the accuracy of the information contained in this YouTube channel or in links to other YouTube channels or websites.  You should not act or rely on any of the information contained herein without seeking advice from a qualified health care provider.</p>
     <h1 className="font-medium mb-1 mt-3">RESULTS AND TESTIMONIALS DISCLAIMER</h1>
     <p>The Site may contain testimonials by users of our products and/or services. These testimonials reflect the real-life experiences and opinions of such users. However, the experiences are totally personal to those particular users, and may not necessarily be representative of all users of our products and/or services. We do not claim, and you should not assume, that all users will have the same experiences. YOUR INDIVIDUAL RESULTS MAY VARY. The Testimonials on the Site are submitted in various forms such as text, audio, and/or video, and are reviewed by us before being posted. They appear on the Site verbatim as given by the users, except for the correction of grammar or typing errors. Some Testimonials may have been shortened for the sake of brevity where the full testimonial contained extraneous information not relevant to the general public. The views and opinions contained in the testimonials belong solely to the individual user and do not reflect our views and opinions. We are not affiliated with users who provide testimonials, and users are not paid or otherwise compensated for their testimonials.</p>
-    <h1 className="font-medium mb-1 mt-3"></h1>
-    <p></p>
-    <h1 className="font-medium mb-1 mt-3"></h1>
-    <p></p>
-    <h1 className="font-medium mb-1 mt-3"></h1>
-    <p></p>
-    <h1 className="font-medium mb-1 mt-3"></h1>
-    <p></p>
+    <h1 className="font-medium mb-1 mt-3">MEDICAL AND MENTAL HEALTH DISCLAIMER</h1>
+    <p>All information, content, and material of this website and/or social media channel (and associated video, audio, visual, and text content) is for general educational and entertainment purposes only and is not a substitute for advice, diagnosis, or treatment regarding medical or mental health conditions.  Always consult a qualified health care provider before making any decisions related to your physical or mental health.  Further, our content and/or resources should not be the sole intervention for any disorder and is not recommended for persons experiencing mental disorders or illness without first consulting a qualified health care provider.</p>
+    <p className="mb-1 mt-3">If you are in crisis, please contact 911 (in the United States) or the National Suicide Prevention Hotline at <a target="_blank" className="text-blue-700" href="https://988lifeline.org/">https://988lifeline.org/</a> or 1-800-273-TALK (8255) or your local emergency services.</p>
+    <h1 className="font-medium mb-1 mt-3">SAFE USE NOTICE</h1>
+    <p>Always select an environment that is safe and secure for yourself before playing meditation recordings or using other content (video, audio, visual, or text) from ADVISER ELITE.  Never listen to or play meditation recordings while driving or operating equipment.</p>
+    <h1 className="font-medium mb-1 mt-3">FAIR-USE COPYRIGHT DISCLAIMER</h1>
+    <p>Under Section 107 of the Copyright Act 1976, allowance is made for "fair use" for purposes such as criticism, commenting, news reporting, teaching, scholarship, and research. Fair use is a use permitted by copyright statute that might otherwise be infringing. Educational, non-profit, or personal use tips the balance in favor of fair use.</p>
+    <p className="mt-1">1) This video/content has no negative impact on the original works</p>
+    <p className="mt-1">2) This video/content is also for educational and teaching purposes</p>
+    <p className="mt-1">3) It is transformative in nature</p>
+    <h1 className="font-medium mb-1 mt-3">AFFILIATE DISCLAIMER</h1>
+    <p>Keep in mind that we may receive commissions when you click our links and make purchases. However, this does not impact our recommendations, reviews, and comparisons. We are highly selective in our recommended content, services, and products and try our best to keep things fair and balanced in order to help you make the best choice for you.</p>
     </Container>
     </div>
 }

a9662725c37034ce422cdb9399060231ef9451c9
Mark Lund <marklund117@gmail.com>
2024-11-28 15:51:05 -0700

    starting on the first of 4 more specific service pages

--- app/strategic/page.tsx
+++ app/strategic/page.tsx
@@ -1,6 +1,10 @@
-import { Button, Container } from '@mantine/core'
+import { Button, Container, Center } from '@mantine/core'
 
-export default function Strategic () {
-    <Container size="xl">
+export default function Strategic() {
+     return <Container size="xl" className="min-h-[100vh]">
+        <h1 className="text-[40px] text-center pt-11 pb-8">Strategic Advisory Services</h1>
+        <Center>
+        <Button m="20" size="lg">Request a service proposal</Button>
+        </Center>
     </Container>
 }

2d8b53cf7a0f1e36f458c95cd2694c207c88d5fe
Mark Lund <marklund117@gmail.com>
2024-11-30 19:25:16 -0700

    starting form fields

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,7 +1,12 @@
 import { Button, Checkbox, Group, TextInput } from '@mantine/core'
 import { useForm } from '@mantine/form'
+import emailjs from '@emailjs/browser'
 
 export default function ContactForm() {
-    return <div>This is a future form component.</div>
-}
+return <form>
+    <TextInput withAsterisk label="First Name"/>
+    <TextInput withAsterisk label="Last Name"/>
+    <TextInput withAsterisk label="Email" placeholder="your@email.com"/>
 
+</form>
+}

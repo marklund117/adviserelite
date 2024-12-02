e5f12c4c001f04a80b6467526624fc65dbac3487
Mark Lund <marklund117@gmail.com>
2024-12-02 15:47:36 -0700

    basic contact page now FUNCTIONAL

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,10 +2,17 @@
 // import tailwind global CSS
 import '../styles/globals.css'
 import ContactForm from '../../components/ContactForm/ContactForm'
+import { Container } from '@mantine/core'
 
 // here's the main dish
 export default function Contact() {
   return <div className="p-4 min-h-[100vh]">
+    <Container size="sm">
+    <h1 className="text-[40px] font-medium text-center">
+            Contact Us
+      </h1>
     <ContactForm/>
+    <p className="mt-3 mb-10">Or email us directly at <a href="mailto:info@adviserelite.com" className="text-[#0079be] underline">info@adviserelite.com</a></p>
+    </Container>
   </div>
 }
--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,12 +1,51 @@
-import { Button, Checkbox, Group, TextInput } from '@mantine/core'
+'use client'
+import { useRef } from 'react'
+import { Button, Checkbox, Group, Textarea, TextInput } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import emailjs from '@emailjs/browser'
 
 export default function ContactForm() {
-return <form>
-    <TextInput withAsterisk label="First Name"/>
-    <TextInput withAsterisk label="Last Name"/>
-    <TextInput withAsterisk label="Email" placeholder="your@email.com"/>
 
-</form>
+const form = useRef<HTMLFormElement>(null);
+
+const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const firstfield = document.getElementsByName('from_name')[0] as HTMLInputElement
+    const secondfield = document.getElementsByName('from_email')[0] as HTMLInputElement
+    const thirdfield = document.getElementsByName('message')[0] as HTMLInputElement
+
+    if (firstfield.value == '' || secondfield.value == '' || thirdfield.value == '') {
+        alert('Please fill out all fields before submitting.')
+        return
+    }
+
+    if (form.current) {
+        emailjs
+            .sendForm('adviser_gmail', 'template_4lfqbvo', form.current, {
+                publicKey: 'cvgcZAiWCc-Ls3NU1',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    firstfield.value = ''
+                    secondfield.value = ''
+                    thirdfield.value = ''
+                    alert('Your message has been sent!')
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                    alert('Internal server error. Please contact us directly!')
+                },
+            );
+    } else {
+        console.error('Form reference is null')
+    }
+}
+
+    return <form ref={form as React.RefObject<HTMLFormElement>} onSubmit={sendEmail}>
+        <TextInput className="mt-3" withAsterisk label="Name" name="from_name" />
+        <TextInput className="mt-3" withAsterisk label="Email" placeholder="your@email.com" name="from_email" />
+        <Textarea className="mt-3" minRows={4} autosize withAsterisk label="Message" name="message" />
+        <Button type="submit" fullWidth bg="#0079be" mt="20" size="lg">Send Message</Button>
+    </form>
 }
